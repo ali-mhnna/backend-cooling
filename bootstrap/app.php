428bfcddc59a84922e27108898e0686e6ac165de
ali <alilearn27@gmail.com>
2026-03-27 14:17:19 +0300

    Updat

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin.token' => \App\Http\Middleware\AdminTokenMiddleware::class,
+        // ✅ تأكد من هاد
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+    
